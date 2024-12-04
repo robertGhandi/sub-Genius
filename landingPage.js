06fc3a7c5b1efe6dba27d9c4e4f7552bc6ed00f3
robertGhandi <robertzerah@gmail.com>
2024-12-04 16:14:20 +0100

    updated landing page

--- landingPage.js
+++ landingPage.js
@@ -1,14 +1,32 @@
 
 
-
-
-
-
 function SignUp() {
-     window.location.href = "./signup.html"
+	window.location.href = "./signup.html";
 }
 
 function LogIn() {
-    window.location.href = "./login.html"
+	window.location.href = "./login.html";
 }
 
+let currentTestimonial = 0;
+const testimonials = document.querySelectorAll(".testimonial-content");
+
+function showTestimonial(index) {
+	testimonials.forEach((testimonial, i) => {
+		testimonial.classList.remove("active");
+		if (i === index) {
+			testimonial.classList.add("active");
+		}
+	});
+}
+
+function showPrevious() {
+	currentTestimonial =
+		(currentTestimonial - 1 + testimonials.length) % testimonials.length;
+	showTestimonial(currentTestimonial);
+}
+
+function showNext() {
+	currentTestimonial = (currentTestimonial + 1) % testimonials.length;
+	showTestimonial(currentTestimonial);
+}
